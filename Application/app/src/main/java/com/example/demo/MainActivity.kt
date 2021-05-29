cea718cfac98b8b1ac4b234880a112f450718a0a
Devrath <devrath.dev595@gmail.com>
2021-05-29 13:39:34 +0530

    dd

--- Application/app/src/main/java/com/example/demo/MainActivity.kt
+++ Application/app/src/main/java/com/example/demo/MainActivity.kt
@@ -2,10 +2,33 @@ package com.example.demo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.demo.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        setCLickListeners()
     }
+
+    private fun setCLickListeners() {
+        binding.apply {
+            mvvmDemoId.setOnClickListener {
+                // MVVM
+            }
+            mvpDemoId.setOnClickListener {
+                // MVP
+            }
+            mviDemoId.setOnClickListener {
+                // MVI
+            }
+        }
+    }
+
+
 }
