cd6e6a1636c405f4456d9f44bf5a289a8acfecb9
Devrath <devrath.dev595@gmail.com>
2023-11-30 19:45:20 +0530

    refactor

--- Application/app/src/main/java/com/example/demo/architectures/commonlayer/model/Creature.kt
+++ Application/app/src/main/java/com/example/demo/architectures/commonlayer/model/Creature.kt
@@ -2,12 +2,15 @@ package com.example.demo.architectures.commonlayer.model
 
 import androidx.annotation.NonNull
 import androidx.room.Entity
+import androidx.room.Ignore
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "creature_table")
-data class Creature(
-        val attributes: CreatureAttributes = CreatureAttributes(),
-        val hitPoints: Int = 0,
-        @PrimaryKey @NonNull val name: String = "",
-        val drawable: Int = 0
-)
+data class Creature @JvmOverloads constructor(
+        var attributes: CreatureAttributes = CreatureAttributes(),
+        var hitPoints: Int = 0,
+        @PrimaryKey @NonNull var name: String = "",
+        var drawable: Int = 0
+){
+
+}
--- Application/app/src/main/java/com/example/demo/architectures/commonlayer/model/CreatureAttributes.kt
+++ Application/app/src/main/java/com/example/demo/architectures/commonlayer/model/CreatureAttributes.kt
@@ -4,4 +4,6 @@ data class CreatureAttributes(
     val intelligence: Int = 0,
     val strength: Int = 0,
     val endurance: Int = 0
-)
+){
+
+}
