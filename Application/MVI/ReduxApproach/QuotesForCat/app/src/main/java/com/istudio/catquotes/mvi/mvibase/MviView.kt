4a03bfe8dfa2e659edec7708cbf44d43d3a71b1d
Devrath <devrath.dev595@gmail.com>
2024-02-20 08:32:00 +0530

    added renderer

--- Application/MVI/ReduxApproach/QuotesForCat/app/src/main/java/com/istudio/catquotes/mvi/mvibase/MviView.kt
+++ Application/MVI/ReduxApproach/QuotesForCat/app/src/main/java/com/istudio/catquotes/mvi/mvibase/MviView.kt
@@ -2,12 +2,16 @@ package com.istudio.catquotes.mvi.mvibase
 
 import com.istudio.catquotes.mvi.mvibase.interfaces.MviIntent
 import com.istudio.catquotes.mvi.mvibase.interfaces.MviViewState
+import io.reactivex.Observable
 import kotlinx.coroutines.flow.Flow
 
 /**
  * Takes the intent from the view & converts into a view state
+ * View must be able to --->
+ * * *  (1) Provide intents to the view model
+ * * *  (2) Render new state coming from the view model
  */
 interface MviView<I : MviIntent, in S : MviViewState> {
-  fun intents(): Flow<I>
+  fun intents(): Observable<I>
   fun render(state: S)
 }
--- Application/MVI/ReduxApproach/QuotesForCat/app/src/main/java/com/istudio/catquotes/mvi/mvibase/MviViewModel.kt
+++ Application/MVI/ReduxApproach/QuotesForCat/app/src/main/java/com/istudio/catquotes/mvi/mvibase/MviViewModel.kt
@@ -2,12 +2,16 @@ package com.istudio.catquotes.mvi.mvibase
 
 import com.istudio.catquotes.mvi.mvibase.interfaces.MviIntent
 import com.istudio.catquotes.mvi.mvibase.interfaces.MviViewState
+import io.reactivex.Observable
 import kotlinx.coroutines.flow.Flow
 
 /**
  * Takes the intent from the view & converts into a state
+ * View model must be able to --->
+ * * *  (1) Process the intents coming from the view
+ * * *  (2) Provide a stream of states for the view to observe
  */
 interface MviViewModel<I : MviIntent, S : MviViewState> {
-  fun processIntents(intents: Flow<I>)
-  fun states(): Flow<S>
+  fun processIntents(intents: Observable<I>)
+  fun states(): Observable<S>
 }
